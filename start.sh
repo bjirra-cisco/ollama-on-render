9f749e4fb43b8c0e2ad54667dc7858c18e582b0f
Bhanuprakash Jirra <bjirra@cisco.com>
2025-06-04 16:06:26 -0700

    added ollama docker

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Start Ollama listening on 0.0.0.0
 ollama serve --host 0.0.0.0 &
 
-# Wait for server to boot
 sleep 5
 
-# Pull model
 ollama pull gemma:2b
 
-# Keep container running
 tail -f /dev/null
